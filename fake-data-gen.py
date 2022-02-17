ffc1b0ea8b6e5b7ca964a89fb6dd1ad69829f7da
Talha-Altair <cooltalha27@gmail.com>
2022-02-17 18:11:02 +0530

    Updates for 2022-02-17 18:11:02

--- fake-data-gen.py
+++ fake-data-gen.py
@@ -13,6 +13,7 @@ import pandas as pd
 fake = faker.Faker()
 
 NUM_ROWS = 10000
+NUM_MIGRATIONS = 100
 
 
 def generate_data(num_rows):
@@ -34,8 +35,27 @@ def generate_data(num_rows):
     return df
 
 
+def generate_migration_date(num_rows):
+
+    old_city_list = [fake.city() for _ in range(num_rows)]
+    new_city_list = [fake.city() for _ in range(num_rows)]
+
+    df = pd.DataFrame({
+        'old_city': old_city_list,
+        'new_city': new_city_list
+    })
+
+    return df
+
+
 if __name__ == '__main__':
 
-    df = generate_data(NUM_ROWS)
+    # df = generate_data(NUM_ROWS)
 
-    df.to_csv('data/employee_data.csv', index=False)
+    # df.to_csv('data/employee_data.csv', index=False)
+
+    df = generate_migration_date(NUM_MIGRATIONS)
+
+    df.to_csv('data/migration_data.csv', index=False)
+
+    print("Done")
